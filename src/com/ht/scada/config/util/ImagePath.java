75a4b313474797ee3e1d3760867ea201d970b1e2
赵磊 <zhaolei-dlut@163.com>
2013-05-02 10:09:58 +0800

    添加图标样例代码

---
+++ src/com/ht/scada/config/util/ImagePath.java
@@ -0,0 +1,20 @@
+package com.ht.scada.config.util;
+
+/**
+ * 图标路径
+ * @author 赵磊
+ *
+ */
+public interface ImagePath {
+	/********监控对象配置************/
+	/**
+	 * 监控对象主索引
+	 */
+	public static final String MAJOR_TAG_IMAGE = "icons/sample.gif";
+	/**
+	 * 监控对象
+	 */
+	public static final String END_TAG_IMAGE = "icons/sample2.gif";
+	
+
+}
