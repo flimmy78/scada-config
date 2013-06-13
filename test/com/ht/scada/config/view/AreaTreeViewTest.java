9ae6b3220ecdf01941c02924c9b158b79ec3817a
赵磊 <zhaolei-dlut@163.com>
2013-06-13 13:16:48 +0800

    修改配置文件

--- test/com/ht/scada/config/view/AreaTreeViewTest.java
+++ test/com/ht/scada/config/view/AreaTreeViewTest.java
@@ -2,10 +2,17 @@ package com.ht.scada.config.view;
 
 import org.junit.Test;
 
+import com.ht.scada.common.tag.util.VarGroupEnum;
+
 public class AreaTreeViewTest {
 
 	@Test
 	public void testCreatePartControlComposite() {
+		System.out.println(VarGroupEnum.valueOf("DIAN_YC").getValue());
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(VarGroupEnum.valueOf("DIAN_YC").getValue());
 	}
 
 }
